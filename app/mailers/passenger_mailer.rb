2582c58d90b69d5d08c80333441a66e5ffb65b89
Blair Guardia <blairguardia@gmail.com>
2021-07-10 15:34:06 +0900

    Add PassengerMailer views

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+
+    def thank_you_email(passenger, booking)
+        @passenger = passenger
+        @booking = booking
+        mail to: @passenger.email,
+             subject: "Thank you for your booking!"
+    end
 end
